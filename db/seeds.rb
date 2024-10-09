66f987d35668faa756a355bd69cef9f554a0bf77
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-10-09 23:11:56 +0900

    deviceとletter_opener_webを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+3.times do |i|
+  Task.create(title: "Todo#{i + 1}")
+end

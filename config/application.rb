49b00c0cdba384869d7529e402c61ff141c6e436
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-10-31 23:52:54 +0900

    rubocopを実行

--- config/application.rb
+++ config/application.rb
@@ -39,10 +39,10 @@ module Myapp
       allow do
         origins '*'
         resource '*',
-          headers: :any,
-          # expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          methods: [:get, :post, :options, :delete, :put]
+                 headers: :any,
+                 # expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                 expose: %w[access-token expiry token-type uid client],
+                 methods: %i[get post options delete put]
       end
     end
   end

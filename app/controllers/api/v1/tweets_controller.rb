9229c68c5b2090b6cc4cdc5a06fc99045dcdb736
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-11-19 23:04:27 +0900

    rubocopを実行

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,24 +1,30 @@
-class Api::V1::TweetsController < ApplicationController
-  def create
-    tweet = current_api_v1_user.tweets.build(tweet_params)
+# frozen_string_literal: true
 
-    if tweet.save
-      render json: tweet, status: :created
-    else
-      render json: tweet.errors, status: :unprocessable_entity
+module Api
+  module V1
+    class TweetsController < ApplicationController
+      def create
+        tweet = current_api_v1_user.tweets.build(tweet_params)
+
+        if tweet.save
+          render json: tweet, status: :created
+        else
+          render json: tweet.errors, status: :unprocessable_entity
+        end
+        # render json: current_api_v1_user
+      end
+
+      def attach_image # rubocop:disable all
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render json: tweet.image
+      end
+
+      private
+
+      def tweet_params
+        params.require(:tweet).permit(:content, :image)
+      end
     end
-    # render json: current_api_v1_user
-  end
-
-  def attach_image
-    tweet = Tweet.find(params[:id])
-    tweet.update(tweet_params)
-    render json: tweet.image
-  end
-
-  private
-
-  def tweet_params
-    params.require(:tweet).permit(:content, :image)
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Tweet, type: :model do
+RSpec.describe Tweet do
   pending "add some examples to (or delete) #{__FILE__}"
 end

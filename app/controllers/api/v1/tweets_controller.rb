4a0e256b1de83923259070cb9512498f0d0b27c4
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2025-01-10 00:33:02 +0900

    指摘点を修正

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -59,7 +59,7 @@ module Api
 
       def comments_index # rubocop:disable all
         tweet = Tweet.find(params[:id])
-        comments = tweet.comments.eager_load(:user)
+        comments = tweet.comments.eager_load(user: { thumbnail_attachment: :blob })
         comments = comments.map do |comment|
           thumbnail = url_for(comment.user.thumbnail) if comment.user.thumbnail.attached?
           { comment:, user: comment.user, thumbnail: }

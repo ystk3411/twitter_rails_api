47ea767e67c81ce95f843b6715b8ffb50e7dc4d5
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2025-01-11 11:15:38 +0900

    rubocopを実行

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,11 +1,17 @@
-class Api::V1::CommentsController < ApplicationController
-  def index
-    tweet = Tweet.find(params[:id])
-    comments = tweet.comments.eager_load(user: { thumbnail_attachment: :blob })
-    comments = comments.map do |comment|
-      thumbnail = url_for(comment.user.thumbnail) if comment.user.thumbnail.attached?
-      { comment:, user: comment.user, thumbnail: }
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def index
+        tweet = Tweet.find(params[:id])
+        comments = tweet.comments.eager_load(user: { thumbnail_attachment: :blob })
+        comments = comments.map do |comment|
+          thumbnail = url_for(comment.user.thumbnail) if comment.user.thumbnail.attached?
+          { comment:, user: comment.user, thumbnail: }
+        end
+        render json: comments
+      end
     end
-    render json: comments
   end
 end

705d2aab35dc452d279fbd263f81bb4a09d832e5
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2025-01-18 23:58:27 +0900

    rubocopを実行

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class RelationshipsController < ApplicationController
-      def create
+      def create # rubocop:disable Metrics/AbcSize
         relationship = Relationship.create(follower_id: current_api_v1_user.id,
                                            followed_id: params[:user_id])
         relationship.save
@@ -11,19 +11,19 @@ module Api
         is_follow = true if user.passive_relationships.find_by(followed_id: params[:user_id])
         count_follow = user.active_relationships.count
         count_followers = user.passive_relationships.count
-        render json: {is_follow:, count_follow:, count_followers:}
+        render json: { is_follow:, count_follow:, count_followers: }
       end
 
-      def destroy
+      def destroy # rubocop:disable Metrics/AbcSize
         relationship = Relationship.find_by(follower_id: current_api_v1_user.id,
-                                           followed_id: params[:user_id])
+                                            followed_id: params[:user_id])
         relationship.destroy
         user = User.find(params[:user_id])
         is_follow = false unless user.passive_relationships.find_by(followed_id: params[:user_id])
-        p Relationship.all
+        Rails.logger.debug Relationship.all
         count_follow = user.active_relationships.count
         count_followers = user.passive_relationships.count
-        render json: {is_follow:, count_follow:, count_followers:}
+        render json: { is_follow:, count_follow:, count_followers: }
       end
     end
   end
